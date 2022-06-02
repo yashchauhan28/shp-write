f276ad855732ce8111732de3a027371c634ea226
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2022-06-03 03:52:35 +0530

    Update download.js

--- src/download.js
+++ src/download.js
@@ -11,7 +11,8 @@ function str2bytes (str) {
 
 module.exports = function(gj, options) {
     var content = zip(gj, options);
-    var blob = new Blob([str2bytes(content)], {type: 'application/zip'});
-    //location.href = 'data:application/zip;base64,' + content;
+//     var blob = new Blob([str2bytes(content)], {type: 'application/zip'});
+//     location.href = 'data:application/zip;base64,' + content;
+    var blob = new Blob([btoa(content)], {type: 'application/zip'});
     saveAs(blob,"hello.zip");
 };
