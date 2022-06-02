ec6826b5431a5b721ef8551bb3bb722891125f59
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2022-06-03 03:32:37 +0530

    Update download.js

--- src/download.js
+++ src/download.js
@@ -3,7 +3,7 @@ var saveAs = require('./filesaver');
 
 module.exports = function(gj, options) {
     var content = zip(gj, options);
-    var blob = new Blob(content, {type: 'application/zip'});
+    var blob = new Blob([content], {type: 'application/zip'});
     //location.href = 'data:application/zip;base64,' + content;
     saveAs(blob,"hello.zip");
 };
