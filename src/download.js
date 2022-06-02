81151b24dd65b684f61c492071532182106b90e8
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2022-06-03 03:43:38 +0530

    Update download.js

--- src/download.js
+++ src/download.js
@@ -1,9 +1,17 @@
 var zip = require('./zip');
 var saveAs = require('./filesaver');
 
+function str2bytes (str) {
+    var bytes = new Uint8Array(str.length);
+    for (var i=0; i<str.length; i++) {
+        bytes[i] = str.charCodeAt(i);
+    }
+    return bytes;
+}
+
 module.exports = function(gj, options) {
     var content = zip(gj, options);
-    var blob = new Blob([content], {type: 'application/zip'});
+    var blob = new Blob([str2bytes(content)], {type: 'application/zip'});
     //location.href = 'data:application/zip;base64,' + content;
     saveAs(blob,"hello.zip");
 };
