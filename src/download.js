7586d7aca59703d753ac7c64566fc11a20df74dc
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2022-06-03 18:51:38 +0530

    Update download.js

--- src/download.js
+++ src/download.js
@@ -9,11 +9,32 @@ function str2bytes (str) {
     return bytes;
 }
 
+const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
+  const byteCharacters = atob(b64Data);
+  const byteArrays = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    byteArrays.push(byteArray);
+  }
+
+  const blob = new Blob(byteArrays, {type: contentType});
+  return blob;
+}
+
 module.exports = function(gj, options) {
     var content = zip(gj, options);
 //     var blob = new Blob([str2bytes(content)], {type: 'application/zip'});
 //     location.href = 'data:application/zip;base64,' + content;
-    var blob = new Blob([atob(content)], {type: 'application/zip'});
-    saveAs(blob,"hello.zip");
+//     var blob = new Blob([atob(content)], {type: 'application/zip'});
+    var blob = b64toBlob(content,'application/zip');
+    saveAs(blob,'b64.zip');
 //     saveAs(btoa(content),"hello.zip");
 };
