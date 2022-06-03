097fbc3592571171adc883894f036e3f41a0796d
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2022-06-03 19:44:42 +0530

    Update geojson.js

--- src/geojson.js
+++ src/geojson.js
@@ -4,10 +4,26 @@ module.exports.polygon = justType('Polygon', 'POLYGON');
 
 function justType(type, TYPE) {
     return function(gj) {
-        var oftype = gj.features.filter(isType(type));
-        return {
-            geometries: (TYPE === 'POLYGON' || TYPE === 'POLYLINE') ? [oftype.map(justCoords)] : oftype.map(justCoords),
+//         var oftype = gj.features.filter(isType(type));
+//         return {
+//             geometries: (TYPE === 'POLYGON' || TYPE === 'POLYLINE') ? [oftype.map(justCoords)] : oftype.map(justCoords),
 //             geometries: (TYPE === 'POLYGON' || TYPE === 'POLYLINE') ? oftype.map(justCoords) : oftype.map(justCoords),
+//             properties: oftype.map(justProps),
+//             type: TYPE
+//         };
+        
+        var oftype = gj.features.filter(isType(type));
+        var geometries;
+        if (TYPE === 'POLYGON') {
+            geometries = [oftype.map(justCoords)];
+        } else if (TYPE === 'POLYLINE') {
+            geometries = oftype.map(function(t) { return [justCoords(t)]; });
+        } else {
+            geometries = oftype.map(justCoords);
+        }
+        
+        return {
+            geometries: geometries,
             properties: oftype.map(justProps),
             type: TYPE
         };
